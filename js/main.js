f94922c42f32521556acf9c9365ba719508a57c0
Ben-Ardiel <45043426+Ben-Ardiel@users.noreply.github.com>
2019-05-14 19:06:43 -0400

    bb
    bbbbb

--- js/main.js
+++ js/main.js
@@ -15,3 +15,21 @@ function move() {
     }
   }
 }
+function refreshMeme(){
+  document.getElementById("Cur") = "*{cursor: url(http://www.rw-designer.com/cursor-extern.php?id=1348), default; }";
+}
+function piglatin(){
+  console.log("Enter your phrase");
+  var a = prompt(">");
+  var newa = a.split(" ");
+  var y = "ay";
+  var newb = "No u";
+  for (i=0;i=newa.length;i++){
+    if (newa[i].length > 3){
+      newa[i].charAt(0) = x;
+      newa[i].slice(1) = newb;
+      newa = newb + x + y;
+      console.log(newa);
+    }
+  }
+}
